69b276be70e8f54b22e8173eff19fad59e6e5d25
Emily Bowe <ebowe@tcd.ie>
2021-02-08 14:57:15 +0000

    Initial commit

---
+++ src/main/kotlin/SmartContract.kt
@@ -0,0 +1,7 @@
+import java.time.LocalDate
+class SmartContract {
+    var closingDate = LocalDate.parse("2021-12-31")
+    var landlordName: String = ""
+    var tenantName: String = ""
+    var depositAgreed: Double = 0.00
+}
